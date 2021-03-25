2d517688275d3829ada2dd920f27022a9633c64c
Jean Michel Rouly <michel.rouly@gmail.com>
2021-03-25 11:28:21 -0400

    redirect stderr to stdout

--- src/scalafmt.ts
+++ src/scalafmt.ts
@@ -26,14 +26,14 @@ async function install(version: string): Promise<void> {
   core.startGroup(`Install scalafmt-native:${version}`)
   const installerUrl =
     'https://raw.githubusercontent.com/scalameta/scalafmt/master/bin/install-scalafmt-native.sh'
-  const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath}`
-  const {stderr} = await exec(cmd)
-  core.info(stderr)
+  const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath} 2>&1`
+  const {stdout} = await exec(cmd)
+  core.info(stdout)
   core.endGroup()
 }
 
 async function execute(args: string): Promise<string> {
-  core.startGroup(`scalafmt-native ${args}`)
+  core.startGroup(`scalafmt-native ${args} 2>&1`)
   const {stdout} = await exec(`${scalafmtPath} ${args}`)
   core.endGroup()
   core.info(stdout)
