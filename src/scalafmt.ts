5b43dd86fd0aa86882f11c515e59969068aac368
Michel Rouly <michel.rouly@gmail.com>
2021-03-25 11:49:52 -0400

    Handle error logging output appropriately. (#4)
    * test ci
    
    * redirect stderr to stdout
    
    * actually redirect output
    
    * try/catch
    
    * log stdout
    
    * dont log as error
    
    * prepare for prime time

--- src/scalafmt.ts
+++ src/scalafmt.ts
@@ -26,16 +26,22 @@ async function install(version: string): Promise<void> {
   core.startGroup(`Install scalafmt-native:${version}`)
   const installerUrl =
     'https://raw.githubusercontent.com/scalameta/scalafmt/master/bin/install-scalafmt-native.sh'
-  const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath}`
-  const {stderr} = await exec(cmd)
-  core.info(stderr)
+  const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath} 2>&1`
+  const {stdout} = await exec(cmd)
+  core.info(stdout)
   core.endGroup()
 }
 
 async function execute(args: string): Promise<string> {
   core.startGroup(`scalafmt-native ${args}`)
-  const {stdout} = await exec(`${scalafmtPath} ${args}`)
-  core.endGroup()
-  core.info(stdout)
-  return stdout
+  try {
+    const {stdout} = await exec(`${scalafmtPath} ${args}`)
+    core.info(stdout)
+    core.endGroup()
+    return stdout
+  } catch (error) {
+    core.info(error.stdout.toString())
+    core.endGroup()
+    return Promise.reject(error)
+  }
 }
