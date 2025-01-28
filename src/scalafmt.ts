2466d4041ee08e8d9d91360d94cefe202c25c97b
Michel Rouly <michel@rouly.net>
2025-01-28 15:15:47 -0800

    prettier

--- src/scalafmt.ts
+++ src/scalafmt.ts
@@ -38,8 +38,7 @@ async function setup(): Promise<void> {
 
 async function install(version: string): Promise<void> {
   core.startGroup(`Install scalafmt-native:${version}`)
-  const installerUrl =
-    `https://raw.githubusercontent.com/scalameta/scalafmt/refs/tags/v${defaultVersion}/bin/install-scalafmt-native.sh`
+  const installerUrl = `https://raw.githubusercontent.com/scalameta/scalafmt/refs/tags/v${defaultVersion}/bin/install-scalafmt-native.sh`
   const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath} 2>&1`
   const {stdout} = await exec(cmd)
   core.info(stdout)
