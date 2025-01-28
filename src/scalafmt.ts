01f5695918b27b2eada29aec74c75060ad7e82a5
Michel Rouly <michel@rouly.net>
2025-01-28 15:11:46 -0800

    get the installer script hopefully working again

--- src/scalafmt.ts
+++ src/scalafmt.ts
@@ -39,7 +39,7 @@ async function setup(): Promise<void> {
 async function install(version: string): Promise<void> {
   core.startGroup(`Install scalafmt-native:${version}`)
   const installerUrl =
-    'https://raw.githubusercontent.com/scalameta/scalafmt/master/bin/install-scalafmt-native.sh'
+    `https://raw.githubusercontent.com/scalameta/scalafmt/refs/tags/v${version}/bin/install-scalafmt-native.sh`
   const cmd = `curl -sL ${installerUrl} | bash -sv -- ${version} ${scalafmtPath} 2>&1`
   const {stdout} = await exec(cmd)
   core.info(stdout)
